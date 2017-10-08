15be1389ad9e8e812efbf7356519b14afe5e367c
frMaciek <wojciechowski.maciek@gmail.com>
2017-10-08 04:39:33 +0200

    Simple quiz app with EditText fields, RadioButton's and CheckBox-es. Everything works

--- MainActivity.java
+++ MainActivity.java
@@ -2,23 +2,23 @@ package com.example.android.quiz_o_matic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
+import static com.example.android.quiz_o_matic.R.id.user_given_answer_field;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    int quizScore = 0;
+    Boolean allAnswersGiven;
+    String textAnswerResult;
     String endMessage;
-    String extraStuff;
-
+    int mQuizScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void createResult(View view) {
 
-        // Figure out if the user wants takes umbrella
-        CheckBox umbrellaCheckbox = (CheckBox) findViewById(R.id.checkbox_umbrella);
-        boolean hasUmbrella = umbrellaCheckbox.isChecked();
+        // Figure out if the user checks answer one
+        CheckBox answerOneCheckbox = (CheckBox) findViewById(R.id.checkbox_answer_one);
+        boolean hasAnswerOne = answerOneCheckbox.isChecked();
 
-        // Figure out if the user wants takes sun filter
-        CheckBox sunFilterCheckbox = (CheckBox) findViewById(R.id.checkbox_sun_filter);
-        boolean hasSunFilter = sunFilterCheckbox.isChecked();
+        // Figure out if the user checks answer two
+        CheckBox answerTwoCheckbox = (CheckBox) findViewById(R.id.checkbox_answer_two);
+        boolean hasAnswerTwo = answerTwoCheckbox.isChecked();
 
-        // Figure out if the user wants takes swiss army knife
-        CheckBox swissArmyCheckbox = (CheckBox) findViewById(R.id.checkbox_swiss_army);
-        boolean hasSwissArmyKnife = swissArmyCheckbox.isChecked();
+        // Figure out if the user checks answer three
+        CheckBox answerThreeCheckbox = (CheckBox) findViewById(R.id.checkbox_answer_three);
+        boolean hasAnswerThree = answerTwoCheckbox.isChecked();
 
         RadioButton q1Yes = (RadioButton) findViewById(R.id.q1Yes);
         boolean q1Agree = q1Yes.isChecked();
@@ -51,92 +51,98 @@ public class MainActivity extends AppCompatActivity {
         RadioButton q5Yes = (RadioButton) findViewById(R.id.q5Yes);
         boolean q5Agree = q5Yes.isChecked();
         RadioButton q1No = (RadioButton) findViewById(R.id.q1No);
-        boolean q1Disagree = q1No.isChecked();
         RadioButton q2No = (RadioButton) findViewById(R.id.q2No);
-        boolean q2Disagree = q2No.isChecked();
         RadioButton q3No = (RadioButton) findViewById(R.id.q3No);
-        boolean q3Disagree = q3No.isChecked();
         RadioButton q4No = (RadioButton) findViewById(R.id.q4No);
-        boolean q4Disagree = q4No.isChecked();
         RadioButton q5No = (RadioButton) findViewById(R.id.q5No);
-        boolean q5Disagree = q5No.isChecked();
 
+        // checking if EditText field has any value entered by user
 
         EditText nameField = (EditText) findViewById(R.id.user_name_field);
-        Editable nameEditable = nameField.getText();
-        String userName = nameEditable.toString();
+        String userName = nameField.getText().toString();
+
+        EditText usersAnswer = (EditText) findViewById(user_given_answer_field);
+        String userGivenAnswer = usersAnswer.getText().toString();
 
 
         /** checking if all answers are given */
-        if (q1Yes.isChecked() || q2Yes.isChecked() || q3Yes.isChecked() || q4Yes.isChecked() || q5Yes.isChecked() || q1No.isChecked() || q2No.isChecked() || q3No.isChecked() || q4No.isChecked() || q5No.isChecked()) {
+        if ((q1Yes.isChecked() || q1No.isChecked()) && (q2Yes.isChecked() || q2No.isChecked()) && (q3Yes.isChecked() || q3No.isChecked()) && (q4Yes.isChecked() || q4No.isChecked()) && (q5Yes.isChecked() || q5No.isChecked()) && (answerOneCheckbox.isChecked() || answerTwoCheckbox.isChecked() || answerThreeCheckbox.isChecked())) {
             Log.d("QAOD", "Answer is given");
+            allAnswersGiven = true;
         } else {
-            Toast.makeText(getApplicationContext(), getString(R.string.toast_answer), Toast.LENGTH_SHORT).show();
+
             Log.d("QAOD", "Answer is Null");
+            allAnswersGiven = false;
         }
-
-        displayMessage(createResultSummary(quizScore, userName, q1Agree, q2Agree, q3Agree, q4Agree, q5Agree, endMessage, hasUmbrella, hasSunFilter, hasSwissArmyKnife));
-
+        displayMessage(createResultSummary(mQuizScore, userName, q1Agree, q2Agree, q3Agree, q4Agree, q5Agree, endMessage, hasAnswerOne, hasAnswerTwo, hasAnswerThree, userGivenAnswer));
     }
 
     public void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.quiz_result);
-        priceTextView.setText(message);
+        if (allAnswersGiven) {
+            // if all answers are given results will be displayed
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), getString(R.string.toast_answer), Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private String createResultSummary(int quizScore, String userName, boolean q1Agree, boolean q2Agree, boolean q3Agree, boolean q4Agree, boolean q5Agree, String endMessage, boolean hasUmbrella, boolean hasSunFilter, boolean hasSwissArmyKnife) {
+    //Creating a result
+    private String createResultSummary(int mQuizScore, String userName, boolean q1Agree, boolean q2Agree, boolean q3Agree, boolean q4Agree, boolean q5Agree, String endMessage, boolean hasAnswerOne, boolean hasAnswerTwo, boolean hasAnswerThree, String userGivenAnswer) {
 
-
-        if (q1Agree == true) {
-            quizScore = quizScore + 1;
+        if (q1Agree) {
+            mQuizScore += 1;
         }
 
         if (q2Agree) {
-            quizScore = quizScore + 1;
+            mQuizScore += 1;
         }
 
         if (q3Agree) {
-            quizScore = quizScore + 1;
+            mQuizScore += 1;
         }
 
         if (q4Agree) {
-            quizScore = quizScore + 1;
+            mQuizScore += 1;
         }
 
         if (q5Agree) {
-            quizScore = quizScore + 1;
+            mQuizScore += 1;
+        }
+        if (hasAnswerOne) {
+            mQuizScore += 2;
         }
 
-        if (quizScore < 3) {
+        if (hasAnswerTwo) {
+            mQuizScore += 1;
+        }
 
-            String wellDoneMessage = getString(R.string.score_low);
-            endMessage = wellDoneMessage.toString();
+        if (hasAnswerThree) {
+            mQuizScore += 0;
+        }
 
-        } else if (quizScore == 3) {
-            String wellDoneMessage = getString(R.string.score_med);
-            endMessage = wellDoneMessage.toString();
+        if (mQuizScore < 3) {
+            endMessage = getString(R.string.score_low);
+        } else if (mQuizScore == 3) {
+            endMessage = getString(R.string.score_med);
         } else {
-            String wellDoneMessage = getString(R.string.score_high);
-            endMessage = wellDoneMessage.toString();
+            endMessage = getString(R.string.score_high);
         }
 
-        if (hasUmbrella) {
-            extraStuff = getString(R.string.checkbox_umbrella);
+        // checking answer given by the user in question seven
+        boolean correct = "twelve".equals(userGivenAnswer.toLowerCase(Locale.getDefault()));
+
+        if (correct) {
+            textAnswerResult = getString(R.string.text_field_string_good_a) + " " + userGivenAnswer + " " + getString(R.string.text_field_string_good);
         } else {
-            extraStuff = " ";
-        }
-        if (hasSunFilter) {
-            extraStuff = extraStuff + " , " + getString(R.string.checkbox_sun_filter);
-        }
-        if (hasSwissArmyKnife) {
-            extraStuff = extraStuff + " , " + getString(R.string.checkbox_swiss_knife);
+            textAnswerResult = getString(R.string.text_field_string_bad_a) + " " + userGivenAnswer + " " + getString(R.string.text_field_string_bad);
         }
 
+        //creating final message with quiz results
         String resultMessage = getString(R.string.congrats) + " " + userName;
-        resultMessage = resultMessage + "\n" + getString(R.string.achieve) + " " + quizScore + " " + getString(R.string.points);
+        resultMessage = resultMessage + "\n" + getString(R.string.achieve) + " " + mQuizScore + " " + getString(R.string.points);
+        resultMessage = resultMessage + "\n" + " " + textAnswerResult;
         resultMessage = resultMessage + "\n" + " " + endMessage;
-        resultMessage = resultMessage + "\n" + " " + getString(R.string.stuff_string) + extraStuff;
-
+//
         return resultMessage;
     }
 }
